219b456de01c59c294ea99b62ce0e5367ee62746
DimitarChristoff <christoff@gmail.com>
2013-11-04 12:02:36 +0000

    saving hashes

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -1,14 +1,22 @@
 define(function(require){
 	'use strict';
 
-	var particleText = require('../../src/drip');
+	String.rot13 = function(str){
+		return str.replace(/[a-zA-Z]/g, function(c){
+			return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+		});
+	};
 
-	var pt = new particleText(document.getElementById('freezer'), {
-		text: 'MONDAY SUCKS'
-	});
+	var particleText = require('../../src/drip'),
+		text = decodeURIComponent(window.location.hash),
+		pt = new particleText(document.getElementById('freezer'), {
+			text: text ? String.rot13(text) : 'MONDAY SUCKS'
+		});
 
 	document.getElementById('setter').addEventListener('change', function(){
-		pt.setText(this.value.toUpperCase());
+		var val = this.value.toUpperCase();
+		pt.setText(val);
+		window.location.hash = encodeURIComponent(String.rot13(val));
 	}, false);
 
 	document.getElementById('stop').addEventListener('click', function(){
